db8f8a9552aa3ad96a0010f75271ad0c5e73bb4c
incessantmeraki <incessantmeraki@gmail.com>
2017-07-15 13:36:50 +0545

    fix for negative numbers

--- index.js
+++ index.js
@@ -1,22 +1,22 @@
 module.exports = function (num1 , num2) {
   result = []
+  isNeg = (num1[0] === '-' && num2[0] !== '-') ||
+          (num2[0] === '-' && num1[0] !== '-')
   num1 = processNumber(num1)
   num2 = processNumber(num2)
-  isNeg = (num1[0] < 0 && num2[0] >=0) ||
-          (num2[0] < 0 && num1[0] >=0)
 
   for ( var i =0; i < num1.length + num2.length; i++) 
     result[i] = 0
     
   for (var i  = 0; i < num1.length; i++) {
     for (var j = 0; j < num2.length; j++) {
-      result[i+j] += num1[i] * num2[j]
+      result[i+j] += parseInt(num1[i] * num2[j])
       result[i+j+1] += parseInt(result[i+j] / 10)
       result [i+j] %= 10
     }
   }
 
-  while (result.length !== 0 && result[result.length-1] === 0) {
+  while (result.length !== 1 && result[result.length-1] === 0) {
     result.splice(result.length-1, 1)
   }
 
@@ -29,8 +29,10 @@ module.exports = function (num1 , num2) {
 
 
 function processNumber(num) {
+  num = num.split('')
+  if (num[0] === '-')
+    num.splice(0,1)
   return num
-    .split('')
     .reverse()
     .map(function(n) {
       return parseInt(n)
